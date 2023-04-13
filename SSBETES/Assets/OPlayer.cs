3ea8b3f34ef2dde8d41f447660f652c2f9f0a908
GoldFix1 <130508249+GoldFix1@users.noreply.github.com>
2023-04-13 23:04:20 +0200

    Add files via upload

---
+++ SSBETES/Assets/OPlayer.cs
@@ -0,0 +1,86 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using DBZGoatLib;
+using System;
+using DBZGoatLib.Model;
+using Microsoft.Xna.Framework;
+using Steamworks;
+using SSBETES.Transformations;
+using System.Security.Policy;
+using Terraria.ModLoader.IO;
+
+namespace SSBETES.Assets
+{
+    internal class OPlayer : ModPlayer
+    {
+        public bool SSBEActive;
+        public bool SSBEUnlockMsg;
+        public bool SSBEAchieved;
+
+        public DateTime? Offset = null;
+
+        public DateTime? PoweringUpTime = null;
+        public DateTime? LastPowerUpTick = null;
+
+        private TransformationInfo? Form = null;
+
+        public override void PostUpdate()
+        {
+            if (SSBEAchieved && !SSBEUnlockMsg)
+            {
+                SSBEUnlockMsg = true;
+                Main.NewText("Your pride unlocked your true potential. Super Saiyan Blue Evolution has been achieved!");
+            }
+
+        }
+
+        public override void SaveData(TagCompound tag)
+        {
+            tag.Add("SSBETES_SSBEAchieved", SSBEAchieved);
+            tag.Add("SSBETES_SSBEUnlockMsg", SSBEUnlockMsg);
+        }
+
+        public override void LoadData(TagCompound tag)
+        {
+            if (tag.ContainsKey("SSBETES_SSBEAchieved"))
+                SSBEAchieved = tag.GetBool("SSBETES_SSBEAchieved");
+            if (tag.ContainsKey("SSBETES_SSBEUnlockMsg"))
+                SSBEUnlockMsg = tag.GetBool("SSBETES_SSBEUnlockMsg");
+        }
+
+        public class BSSFPanel : TransformationTree
+        {
+            public override bool Complete() => true;
+
+            public override bool Condition(Player player)
+            {
+                var Oplayer = player.GetModPlayer<OPlayer>();
+                return player.GetModPlayer<GPlayer>().Trait == "Prodigy";
+            }
+
+            public override Connection[] Connections() => new Connection[]
+            {
+            new Connection(0,2,1,false,new Gradient(Color.RoyalBlue).AddStop(0.60f, new Color(0, 0, 255))),
+            };
+
+            public override string Name() => "Bonus Forms";
+
+            public override Node[] Nodes() => new Node[]
+            {
+            new Node(0, 2, "SSBEBuff", "SSBETES/Transformations/SSBEBuff", "Train harder in your god form",UnlockConditionSSBE,DiscoverConditionSSBE),
+            };
+            public bool UnlockConditionSSBE(Player player)
+            {
+                var Oplayer = player.GetModPlayer<OPlayer>();
+                return Oplayer.SSBEAchieved;
+            }
+
+            public bool DiscoverConditionSSBE(Player player)
+            {
+                return true;
+            }
+
+
+        }
+    }
+}
---
+++ SSBETES/Transformations/SSBEBuff.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using DBZGoatLib.Model;
+using Microsoft.Xna.Framework.Graphics;
+using DBZGoatLib;
+using Terraria.ModLoader;
+using DBZGoatLib.Handlers;
+using SSBETES.Assets;
+
+namespace SSBETES.Transformations
+{
+    internal class SSBEBuff : Transformation
+    {
+        public override AuraData AuraData() => new AuraData("SSBETES/Assets/SSBEAura", 4, BlendState.AlphaBlend, new Color(250, 74, 67));
+
+        public override bool CanTransform(Player player)
+        {
+            var modPlayer = player.GetModPlayer<OPlayer>();
+
+            return !player.HasBuff<SSBEBuff>() && modPlayer.SSBEAchieved;
+        }
+
+        public override string FormName() => "Super Saiyan Blue Evolution";
+
+        public override string HairTexturePath() => "SSBETES/Assets/SSBEHair";
+
+        public override Gradient KiBarGradient() => new Gradient(new Color(11, 63, 165)).AddStop(1f, new Color(20, 47, 127));
+
+        public override void OnTransform(Player player)
+        {
+            player.GetModPlayer<OPlayer>().SSBEActive = true;
+        }
+
+        public override void PostTransform(Player player)
+        {
+            player.GetModPlayer<OPlayer>().SSBEActive = false;
+        }
+
+        public override bool SaiyanSparks() => false;
+
+        public override SoundData SoundData() => new SoundData("DBZMODPORT/Sounds/SSJAscension", "DBZMODPORT/Sounds/SSJ3", 260);
+
+        public override bool Stackable() => false;
+
+        public override Color TextColor() => Color.RoyalBlue;
+
+        public override void SetStaticDefaults()
+        {
+            damageMulti = 5.1f;
+            speedMulti = 3.6f;
+
+            kiDrainRate = 5.0f;
+            kiDrainRateWithMastery = 2.0f;
+
+            attackDrainMulti = 1.9f;
+            baseDefenceBonus = 51;
+
+            base.SetStaticDefaults(); 
+        }
+    }
+}
